f1470efbd83641437ea078b947086377896580fe
k.bitkov <k.bitkov@corp.mail.ru>
2024-02-21 23:01:02 +0300

    order screen in progress

--- composeApp/src/commonMain/kotlin/presentation/orders/OrdersScreen.kt
+++ composeApp/src/commonMain/kotlin/presentation/orders/OrdersScreen.kt
@@ -1,16 +1,21 @@
 package presentation.orders
 
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsFocusedAsState
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.selection.selectable
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableIntState
-import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import presentation.components.HeaderComponent
+import presentation.components.UserInfoComponent
 import presentation.navigation.Destinations
 
 private const val ORDER_DETAILS_POSITION = 0
@@ -23,14 +28,22 @@ fun OrdersScreen(
 ) {
     val selectedItemPosition = mutableIntStateOf(0)
 
-    Row(modifier = modifier) {
-        CategoriesColumn(
-            modifier = Modifier.weight(1f),
-            selectedItemPosition = selectedItemPosition,
+    Column(modifier = modifier) {
+        HeaderComponent(
+            onLogoClick = { onScreenChanged(Destinations.Home) },
+            onOrdersClick = { onScreenChanged(Destinations.OrdersHistory) },
+            onAccountClick = { onScreenChanged(Destinations.Account) }
         )
-        when (selectedItemPosition.intValue) {
-            ORDER_DETAILS_POSITION -> OrderDetails()
-            EXPERTS_LIST_POSITION -> ExpertsList()
+
+        Row {
+            CategoriesColumn(
+                modifier = Modifier.weight(1f),
+                selectedItemPosition = selectedItemPosition,
+            )
+            when (selectedItemPosition.intValue) {
+                ORDER_DETAILS_POSITION -> OrderDetails(modifier = Modifier.weight(2f).fillMaxWidth())
+                EXPERTS_LIST_POSITION -> ExpertsList(modifier = Modifier.weight(2f).fillMaxWidth())
+            }
         }
     }
 }
@@ -60,13 +73,36 @@ private fun CategoriesColumn(
 }
 
 @Composable
-private fun OrderDetails() {
+private fun OrderDetails(modifier: Modifier) {
+    var text by remember { mutableStateOf("") }
 
+    Column(modifier = modifier) {
+        Text(text = "Что нужно сделать?")
+
+        TextField(
+            shape = RoundedCornerShape(8.dp),
+            value = text,
+            onValueChange = { text = it },
+            textStyle = MaterialTheme.typography.h6,
+            placeholder = { Text(text = "Опишите специалисту, детали заказа") },
+            colors = TextFieldDefaults.textFieldColors(
+                textColor = MaterialTheme.colors.primary,
+                placeholderColor = MaterialTheme.colors.primaryVariant,
+                backgroundColor = MaterialTheme.colors.onPrimary,
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+            ),
+        )
+
+        Card(backgroundColor = MaterialTheme.colors.onPrimary) {
+            UserInfoComponent()
+        }
+    }
 }
 
 @Composable
-private fun ExpertsList() {
-
+private fun ExpertsList(modifier: Modifier) {
+    Column(modifier = modifier) {  }
 }
 
 @Composable
@@ -76,9 +112,16 @@ private fun SelectableText(
     isSelected: Boolean,
     onClick: () -> Unit,
 ) {
+    val interactionSource = remember { MutableInteractionSource() }
+    val isFocused by interactionSource.collectIsFocusedAsState()
+
     Text(
         modifier = modifier
-            .selectable(selected = isSelected),
+            .selectable(
+                selected = isSelected,
+                enabled = true,
+                onClick = onClick,
+            ),
         text = text,
         style = MaterialTheme.typography.subtitle1,
         color = MaterialTheme.colors.primary,
